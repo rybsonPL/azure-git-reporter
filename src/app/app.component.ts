df20d047371a777be7f2e5448a6a095901f199a3
rybsonPL <103430332+rybsonPL@users.noreply.github.com>
2023-12-16 13:02:35 +0100

    chore/install basic dependencies and config project (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CalendarModule } from 'primeng/calendar';
+
+import { primengConfig } from '@core/prime-ng.config';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CalendarModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'azure-git-reporter';
+  constructor() {
+    primengConfig();
+  }
 }
