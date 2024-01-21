321746697eb1637107dfd506ffddf21ef4cc61fc
Mariusz Rybski <pl.rybson@gmail.com>
2024-01-21 11:43:37 +0100

    chore/add electron

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
+
+function onReady() {
+  win = new BrowserWindow({
+    width: 900,
+    height: 800,
+    minWidth: 375,
+    minHeight: 500,
+    maxWidth: 900,
+    maxHeight: 800,
+    maximizable: false,
+  });
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'dist/azure-git-reporter/browser/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  win.webContents.openDevTools();
+}
+
+app.on('ready', onReady);
