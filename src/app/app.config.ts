7a41a1531136f7e8d1dd710be3e9b77acf274ba5
Mariusz Rybski <pl.rybson@gmail.com>
2024-01-21 10:49:49 +0100

    chore/remove router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,15 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { DialogService } from 'primeng/dynamicdialog';
 
 import { apiVersionInterceptor, authorizationInterceptor, handleErrorsInterceptor } from '@core/interceptors';
 
-import { routes } from './app.routes';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     MessageService,
     DialogService,
-    provideRouter(routes),
     provideAnimations(),
     provideHttpClient(withInterceptors([authorizationInterceptor, apiVersionInterceptor, handleErrorsInterceptor])),
   ],
